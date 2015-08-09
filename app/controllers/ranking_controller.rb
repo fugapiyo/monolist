2ee8b93fab5f2550adcb5dcc7b09a7c49073d54b
fugapiyo <amwtpgaj@softbank.ne.jp>
2015-08-09 08:57:34 +0000

    mod logic: sort

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,6 +1,16 @@
 class RankingController < ApplicationController
   def show
-    @rank_mode = params[:rank_mode].capitalize
-    @items = Item.ranking(@rank_mode, 10)
+    @rank_mode = params[:rank_mode].downcase
+    self.send(@rank_mode)
+  end
+  
+  def have
+    ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @items = Item.find(ids)
+  end
+
+  def want
+    ids = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @items = Item.find(ids)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,23 +10,4 @@ class Item < ActiveRecord::Base
   has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
   has_many :have_users, through: :haves, source: :user
 
-  # Want/Haveランキング情報を降順ソートしてHashで返却する
-  def self.ranking(rank_mode, array_max)
-    # JOIN用のテーブル名シンボルを動的生成
-    join_table_name = "#{rank_mode.downcase}_users".to_sym
-    
-    # item_idとcountの配列を取得
-    rank_array = Item.joins(join_table_name).group('ownerships.item_id').count
-    # カウント数をベースに昇順ソートし、item_idのみの配列に変換
-    rank_array_sorted = rank_array.sort_by{|key,val| -val}.map{|ary| ary[0]}
-    
-    # idからitemオブジェクトの配列を生成
-    max = array_max - 1
-    rank_item_array = rank_array_sorted[0..max].map do |id|
-      Item.find_by(id: id)
-    end
-    
-    rank_item_array
-  end
-
 end
